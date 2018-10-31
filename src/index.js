007a3531c2d969cbf7fb49e44d58076486ccba03
AJClassic2014 <auguestconverse2@gmail.com>
2018-10-31 15:19:51 +0800

    build apple basket app by redux

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import reducer from './redux/reducers/appleBasketReducer.js';
+import AppleBasket from './redux/components/AppleBasket.jsx';
+import './assets/styles/index.css';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+ const renderApp = () => {
+   ReactDOM.render(<Provider store={store}>
+   <AppleBasket />
+   </Provider>,
+   document.getElementById('app'));
+ };
+renderApp();
---
+++ src/redux/actions/appleActions.js
@@ -0,0 +1,43 @@
+const prefix = 'apple/';
+
+let actions = {
+
+  pickApple: () => (dispatch, getState) => {
+    if (getState().picking) {
+      return;
+    }
+    dispatch(actions.stratPickApple());
+    fetch('./src/assets/json/model_60.json')
+      .then(res => {
+        if (res.status != 200) dispatch(errorPickApple(res.statusText));
+        let appleWeight = Math.floor(200 + Math.random() * 50);
+        dispatch(actions.finishPickApple(appleWeight));
+      }).catch(e => {
+        console.log(e)
+        dispatch(actions.errorPickApple(e.statusText));
+      })
+
+  },
+
+  stratPickApple: () => ({
+    type: 'apple/START_PICK_APPLE',
+  }),
+
+  finishPickApple: (appleWeight) => ({
+    type: 'apple/FINISH_PICK_APPLE',
+    payload: appleWeight,
+  }),
+
+  errorPickApple: (msg) => ({
+    type: 'apple/ERROR_PICK_APPLE',
+    payload: new Error(msg),
+    error: true
+  }),
+
+  eatApple: (appleId) => ({
+    type: 'apple/EAT_APPLE',
+    payload: appleId
+  })
+}
+
+export default actions;
